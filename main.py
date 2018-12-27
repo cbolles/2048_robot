4a73a2dd246f1763ad6ce838451ac0630ac989b2
Collin Bolles <collinbolles@gmail.com>
2018-12-27 02:53:55 -0500

    Seperate between game and user

--- main.py
+++ main.py
@@ -1,80 +1,15 @@
-import pygame
-from game_objects import Tile, Stack, TileQueue, ScoreDisplay, DiscardPile
-
-
-# Default Values
-SIZE = (1200, 800)
-NUM_STACKS = 4
-FONT_SIZE = 30
-FRAMERATE = 10
-
-# Initialize Pygame
-pygame.init()
-pygame.font.init()
-font = pygame.font.SysFont('Comic Sans MS', FONT_SIZE)
-screen = pygame.display.set_mode(SIZE)
-pygame.display.set_caption("2048 Solitaire")
-clock = pygame.time.Clock()
-
-# Game Objects
-stacks = []
-tile_queue = TileQueue(60, 650)
-score_display = ScoreDisplay(1000, 0, font)
-discard_pile = DiscardPile(1100, 500)
-
-# Test values
-test_tiles = [Tile(8), Tile(4), Tile(2)]
-
-
-def generate_stacks(startX, startY):
-    stackX = startX
-    stackY = startY
-    distance = SIZE[0] // NUM_STACKS
-    for i in range(0, NUM_STACKS):
-        stacks.append(Stack(stackX, stackY))
-        stackX += distance
-        # Test stacks
-        stacks[i].add_tile(test_tiles)
-
-
-def update_game_objects():
-    screen.fill((0, 0, 0))
-    for stack in stacks:
-        stack.draw(screen)
-    tile_queue.draw(screen)
-    score_display.draw(screen)
-    discard_pile.draw(screen)
-    pygame.display.flip()
-
-
-def stack_change(stack_number):
-    if stack_number < len(stacks):
-        stack = stacks[stack_number]
-        score_change = stack.add_tile(tile_queue.pull())
-        score_display.increase_score(score_change)
-        # 2048 achieved
-        if len(stack) == 0:
-            discard_pile.clear_discards()
-    else:
-        if not discard_pile.pile_full():
-            tile_queue.pull()
-            discard_pile.add_discard()
-    update_game_objects()
+from game_objects import Game
+import os
+from pathlib import Path
+from users import Human
 
 
 def main():
-    # Initalize Game Objects
-    generate_stacks(0, 20)
-    running = True
-    update_game_objects()
-    while running:
-        # Listen for quite request from user
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                stack_change(int(pygame.key.name(event.key)[1]))
-        clock.tick(FRAMERATE)
+    current_dir = Path(os.path.dirname(os.path.abspath(__file__)))
+    config_path = str(current_dir / 'resources' / 'config' / 'basic_ui.ini')
+    game = Game(config_path)
+    human = Human(config_path, game)
+    human.run()
 
 
 if __name__ == '__main__':
---
+++ users.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+import pygame
+from configparser import ConfigParser
+
+
+class User(ABC):
+    def __init__(self, config_location, game):
+        config = ConfigParser()
+        config.read(config_location)
+        self.init_pygame(config)
+        self.game = game
+
+    def init_pygame(self, config):
+        pygame.init()
+        pygame.font.init()
+        self.init_font(config)
+        self.init_screen(config)
+        self.init_clock(config)
+        caption = config['pygame_setup']['app_name']
+        pygame.display.set_caption(caption)
+
+    def init_font(self, config):
+        font_size = int(config['font']['font_size'])
+        font_name = config['font']['font_name']
+        self.font = pygame.font.SysFont(font_name, font_size)
+
+    def init_screen(self, config):
+        screen_size = self.get_pair(config['size']['screen_size'])
+        self.screen = pygame.display.set_mode(screen_size)
+
+    def init_clock(self, config):
+        self.clock = pygame.time.Clock()
+        self.framerate = int(config['pygame_setup']['framerate'])
+
+    def get_pair(self, raw_values):
+        str_values = raw_values.split(',')
+        pos_x = int(str_values[0])
+        pos_y = int(str_values[1])
+        return (pos_x, pos_y)
+
+    @abstractmethod
+    def run(self):
+        pass
+
+
+class Human(User):
+    def run(self):
+        running = True
+        self.game.draw(self.screen, self.font)
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    pile_number = int(pygame.key.name(event.key)[1])
+                    self.game.make_move(pile_number)
+                    self.game.draw(self.screen, self.font)
+            self.clock.tick(self.framerate)
