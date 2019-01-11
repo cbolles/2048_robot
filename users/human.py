39982f2b17b849b8ce7f338d563094054e79ac74
Collin Bolles <collinbolles@gmail.com>
2019-01-11 00:43:52 -0500

    Revert "Refactor/mech/game ui basic"

--- users/human.py
+++ users/human.py
@@ -1,24 +1,16 @@
-from users.base_user import User
 import pygame
+from users.utils import User
 
 
 class Human(User):
-    def __init__(self, config_path):
-        class_name = type(self).__name__
-        params = dict()
-        params['game_display'] = True
-        super(Human, self).__init__(config_path, params, class_name)
-        self.running = True
-
-    def get_target_pile(self, events):
+    def get_move(self, events):
         for event in events:
             if event.type == pygame.KEYDOWN:
                 target_num = int(pygame.key.name(event.key))
-                target_pile = self.game_controller.game_model.get_pile(target_num)
-                return target_pile
-            if event.type == pygame.QUIT:
-                self.running = False
+                target_pile = None
+                if target_num < len(self.game.stacks):
+                    target_pile = self.game.stacks[target_num]
+                else:
+                    target_pile = self.game.discard_pile
+                return self.create_move(target_pile)
         return None
-
-    def is_running(self):
-        return self.running
